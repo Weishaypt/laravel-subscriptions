16965a801cdbf07ca579c397061adea8bd6557da
Alexandr Konyaev <40069228+Weishaypt@users.noreply.github.com>
2020-11-24 09:41:05 +0300

    Update Plan.php

--- src/Models/Plan.php
+++ src/Models/Plan.php
@@ -174,7 +174,7 @@ class Plan extends Model implements Sortable
         $this->setTable(config('weishaypt.subscriptions.tables.plans'));
         $this->setRules([
             'slug' => 'required|alpha_dash|max:150|unique:'.config('weishaypt.subscriptions.tables.plans').',slug',
-            'name' => 'required|string|strip_tags|max:150',
+            'name' => 'required|string|max:150',
             'description' => 'nullable|string|max:10000',
             'is_active' => 'sometimes|boolean',
             'price' => 'required|numeric',
